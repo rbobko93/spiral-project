d44701a5f8003e2e0790aed8903d778a0e925899
Rafal Bobko <rbobko70@gmail.com>
2022-02-26 11:47:41 -0500

    feat(srv): initial commit

---
+++ src/main/java/com/rbobko/spiralproject/SpiralProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rbobko.spiralproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiralProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiralProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbobko/spiralproject/SpiralProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbobko.spiralproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiralProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
